722632fdcda40ef0222abd522fc648a7dce3fca3
Damoiskii <moimyazz@gmail.com>
2026-09-15 13:24:26 -0500

    feat: add MarioScreenV2 with movement controls and animations

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
 import EntranceScreen from "@/screens/EntranceScreen";
-import MarioScreen from "@/screens/MarioScreen";
+import MarioScreen from "@/screens/MarioScreenV2";
 import { StyleSheet, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <EntranceScreen />
-      {/* <MarioScreen /> */}
+      {/* <EntranceScreen /> */}
+      <MarioScreen />
     </View>
   );
 }
